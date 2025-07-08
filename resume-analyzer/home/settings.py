b51cd433054c3a287cd5c048cf84e09eddd36986
sibiraj <sibiraj1003@gmail.com>
2025-07-08 11:28:25 +0530

    Configure settings.py for Render deployment

--- resume-analyzer/home/settings.py
+++ resume-analyzer/home/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p84+z6x5@xm6u70abg%!d(@4ign#33iv@bc$usb2dmleaq1z8f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,6 +73,7 @@ TEMPLATES = [
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 WSGI_APPLICATION = 'home.wsgi.application'
 
